444de82d9d1b32a3dd3ed5c21b605aa47794535a
tmktc <tmktc.git@gmail.com>
2025-04-07 16:34:52 +0200

    feat(login): retrieve user input and ask server for verification

--- Client/src/main/java/cz/vse/pexeso/controller/LoginController.java
+++ Client/src/main/java/cz/vse/pexeso/controller/LoginController.java
@@ -1,23 +1,54 @@
 package cz.vse.pexeso.controller;
 
+import cz.vse.pexeso.model.User;
+import cz.vse.pexeso.model.observer.MessageType;
+import cz.vse.pexeso.network.ClientConnection;
+import cz.vse.pexeso.network.MessageBuilder;
+import cz.vse.pexeso.network.MessageHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginController {
     @FXML
-    private PasswordField passwordField;
-    @FXML
     private TextField usernameField;
     @FXML
-    private Button loginButton;
+    private PasswordField passwordField;
     @FXML
     private Label warningLabel;
 
     @FXML
-    private void handleLoginClick() {
-        //TODO: verify login, if successful, load the lobby, otherwise show warning
+    private void initialize() {
+        MessageHandler.getInstance().register(MessageType.LOGIN_OK, this::handleSuccessfulLogin);
+        MessageHandler.getInstance().register(MessageType.LOGIN_INVALID, this::handleInvalidLogin);
+        MessageHandler.getInstance().register(MessageType.LOGIN_DUPLICATE, this::handleDuplicateLogin);
     }
+
+    @FXML
+    private void handleLoginClick() {
+        if (usernameField.getText().isEmpty() || passwordField.getText().isEmpty()) {
+            warningLabel.setText("Please fill in all fields.");
+        } else {
+            warningLabel.setText("");
+            User user = new User(usernameField.getText(), passwordField.getText());
+            ClientConnection connection = new ClientConnection();
+
+            String loginMessage = MessageBuilder.getInstance().buildLoginMessage(user);
+            connection.sendMessage(loginMessage);
+        }
+    }
+
+    private void handleSuccessfulLogin() {
+        //TODO: load the lobby
+    }
+
+    private void handleInvalidLogin() {
+        warningLabel.setText("Invalid username or password.");
+    }
+
+    private void handleDuplicateLogin() {
+        warningLabel.setText("User already logged in.");
+    }
+
 }
