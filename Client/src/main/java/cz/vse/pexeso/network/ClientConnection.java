8c92f0c6b20f1460d1f9d6823105efad18fc9c54
tmktc <tmktc.git@gmail.com>
2025-04-07 16:30:08 +0200

    feat: implement ClientConnection with sending and listening for messages

---
+++ Client/src/main/java/cz/vse/pexeso/network/ClientConnection.java
@@ -0,0 +1,66 @@
+package cz.vse.pexeso.network;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ClientConnection {
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    public ClientConnection() {
+        try {
+            this.socket = new Socket("localhost", 1234);
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            listenForMessage();
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+            throw new RuntimeException("Error creating client connection", e);
+        }
+    }
+
+    public void sendMessage(String message) {
+        try {
+            bufferedWriter.write(message);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    public void listenForMessage() {
+        new Thread(() -> {
+            String messageFromServer;
+
+            while (socket.isConnected()) {
+                try {
+                    messageFromServer = bufferedReader.readLine();
+                    MessageHandler.getInstance().parseMessage(messageFromServer);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    closeEverything(socket, bufferedReader, bufferedWriter);
+                }
+            }
+        }).start();
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
