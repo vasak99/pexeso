447fdbfac74ba23b2f25a62195727c7993b6e0f2
tmktc <tmktc.git@gmail.com>
2025-04-08 19:59:32 +0200

    feat: add game screen fxml file

--- Client/src/main/java/cz/vse/pexeso/controller/LobbyController.java
+++ Client/src/main/java/cz/vse/pexeso/controller/LobbyController.java
@@ -1,5 +1,11 @@
 package cz.vse.pexeso.controller;
 
+import cz.vse.pexeso.helper.SceneManager;
+
 public class LobbyController {
     //TODO: Implement matchmaking
+
+    private void joinGame() {
+        SceneManager.switchScene("/cz/vse/pexeso/fxml/game.fxml");
+    }
 }
