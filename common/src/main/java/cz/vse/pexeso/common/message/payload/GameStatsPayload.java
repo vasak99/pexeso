7b1d3f87dbd64daf3bd9374b98dbfb8b869e3891
Paul <vasak314159@gmail.com>
2025-06-07 18:54:00 +0200

    #29 Game stats translation

--- common/src/main/java/cz/vse/pexeso/common/message/payload/GameStatsPayload.java
+++ common/src/main/java/cz/vse/pexeso/common/message/payload/GameStatsPayload.java
@@ -1,5 +1,6 @@
 package cz.vse.pexeso.common.message.payload;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,6 +11,18 @@ public class GameStatsPayload implements MessagePayload {
 
     public GameStatsPayload(List<GameStat> games) {
         this.games = games;
+    }
+
+    public GameStatsPayload(String data) {
+        var mapper = new ObjectMapper();
+
+        try {
+            GameStatsPayload rr = mapper.readValue(data, GameStatsPayload.class);
+
+            this.games = rr.games;
+        } catch (Exception e) {
+            this.games = new ArrayList<>();
+        }
     }
 
     @Override
